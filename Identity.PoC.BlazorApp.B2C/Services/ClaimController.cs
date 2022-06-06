8c674a8ee72b34a4eb46bb1c0f9db9e14e383caf
Phanis Parpas <pparpas@hotmail.com>
2022-06-06 20:47:12 +0300

    Basic Auth

--- Identity.PoC.BlazorApp.B2C/Services/ClaimController.cs
+++ Identity.PoC.BlazorApp.B2C/Services/ClaimController.cs
@@ -45,9 +45,11 @@ namespace Identity.PoC.BlazorApp.B2C.Services
             // Get the object id of the user that is signing in.
             //var objectId = body.GetValue("objectId").ToString();
             string secret = _config["AzureAD:EnrichClaimSecret"];
+            string token = _config["AzureAD:EnrichClaimToken"];
+            
             var responseProperties = new Dictionary<string, object>
               {
-                { "extension_33903e226c8a4610b44e2a2265b0e234_CustomClaim", "ClaimAdded" },
+                { "extension_33903e226c8a4610b44e2a2265b0e234_CustomClaim", token },
                 { "extension_33903e226c8a4610b44e2a2265b0e234_CustomClaim2",secret }
               };
 
